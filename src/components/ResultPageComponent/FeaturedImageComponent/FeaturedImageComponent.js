9b83574b57ef3de07ff1307d449daf1b748b64db
suddiyo <suddiyo@naver.com>
2023-10-24 16:38:18 +0900

    Feat: ResultPage에 상단 네비게이션 추가

---
+++ src/components/ResultPageComponent/FeaturedImageComponent/FeaturedImageComponent.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './FeaturedImageComponent.module.css';
+
+
+function FeaturedImageComponent(props) {
+    return (
+        <div className={styles.container}>
+        
+
+        </div>
+    );
+}
+
+export default FeaturedImageComponent;
---
+++ src/pages/ResultPage/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import FlowerResultContainer from '../../components/ResultPageComponent/FlowerResultContainer/FlowerResultContainer';
+import FeaturedImageComponent from '../../components/ResultPageComponent/FeaturedImageComponent/FeaturedImageComponent';
+
+function ResultPage() {
+
+    return (
+        <FlowerResultContainer>
+            <FeaturedImageComponent />
+        </FlowerResultContainer>
+    );
+}
+
+export default ResultPage;
