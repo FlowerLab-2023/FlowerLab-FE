b491e0527d0ad5f5fbcc390e0c743c043ca4582d
suddiyo <suddiyo@naver.com>
2023-10-29 12:20:42 +0900

    Feat: FeatureDetail 구현

--- src/pages/ResultPage/index.js
+++ src/pages/ResultPage/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 
 import FlowerResultContainer from '../../components/ResultPageComponent/FlowerResultContainer/FlowerResultContainer';
 import FeaturedImage from '../../components/ResultPageComponent/FeaturedImage/FeaturedImage';
+import FeatureDetail from '../../components/ResultPageComponent/FeatureDetail/FeatureDetail';
 
 function ResultPage() {
 
     return (
         <FlowerResultContainer>
             <FeaturedImage />
+            <FeatureDetail />
         </FlowerResultContainer>
     );
 }
